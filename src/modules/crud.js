3795f4f8f61529fe7ab8f64c05c05208b8b2a5fd
Htet Naing <htetnaing0814@gmail.com>
2022-07-06 12:45:06 +0630

    fixed linters

--- src/modules/crud.js
+++ src/modules/crud.js
@@ -3,59 +3,60 @@ import Task from './task.js';
 let allTasks;
 
 if (JSON.parse(localStorage.getItem('tasks')) === null) {
-    allTasks = [];
+  allTasks = [];
 } else {
-    allTasks = JSON.parse(localStorage.getItem('tasks'));
+  allTasks = JSON.parse(localStorage.getItem('tasks'));
 }
 
 const addTask = (description) => {
-    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
-    if (storedTasks === null) {
-        allTasks = [];
-    } else {
-        allTasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-    const newTask = new Task(allTasks.length, false, description);
-    allTasks.push(newTask);
-    localStorage.setItem('tasks', JSON.stringify(allTasks));
-    window.location.reload();
+  const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+  if (storedTasks === null) {
+    allTasks = [];
+  } else {
+    allTasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  const newTask = new Task(allTasks.length, false, description);
+  allTasks.push(newTask);
+  localStorage.setItem('tasks', JSON.stringify(allTasks));
+  window.location.reload();
 };
 
 const editTask = (index, description) => {
-    const task = allTasks.find((task) => task.index === index);
-    task.description = description;
-    localStorage.setItem('tasks', JSON.stringify(allTasks));
+  const task = allTasks.find((task) => task.index === index);
+  task.description = description;
+  localStorage.setItem('tasks', JSON.stringify(allTasks));
 };
 
 const toggleTask = (index, completed) => {
-    const task = allTasks.find((task) => task.index === index);
-    task.completed = completed;
-    localStorage.setItem('tasks', JSON.stringify(allTasks));
+  const task = allTasks.find((task) => task.index === index);
+  task.completed = completed;
+  localStorage.setItem('tasks', JSON.stringify(allTasks));
 };
 
 const removeTask = () => {
-    const completedTasks = [];
-    allTasks.forEach((task) => {
-        if (task.completed === false) {
-            completedTasks.push(task);
-        }
-    });
-    for (let i = 0; i < completedTasks.length; i += 1) {
-        completedTasks[i].index = i;
+  const completedTasks = [];
+  allTasks.forEach((task) => {
+    if (task.completed === false) {
+      completedTasks.push(task);
     }
-    allTasks = allTasks.filter(task => {
-        if (completedTasks.includes(task)) {
-            return task
-        }
-    });
-    allTasks = completedTasks;
-    localStorage.setItem('tasks', JSON.stringify(allTasks));
-    window.location.reload();
+  });
+  for (let i = 0; i < completedTasks.length; i += 1) {
+    completedTasks[i].index = i;
+  }
+  allTasks = allTasks.filter((task) => {
+    if (completedTasks.includes(task)) {
+      return task;
+    }
+    return task;
+  });
+  allTasks = completedTasks;
+  localStorage.setItem('tasks', JSON.stringify(allTasks));
+  window.location.reload();
 };
 
 export {
-    addTask,
-    editTask,
-    toggleTask,
-    removeTask,
+  addTask,
+  editTask,
+  toggleTask,
+  removeTask,
 };
